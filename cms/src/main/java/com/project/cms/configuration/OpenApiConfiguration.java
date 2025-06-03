2b933406fcdae187f44acaef0ef9eb7087fb74dd
AqilAzizov2005 <aqilazizov2005@gmail.com>
2025-06-03 18:14:28 +0400

    open api updated

--- cms/src/main/java/com/project/cms/configuration/OpenApiConfiguration.java
+++ cms/src/main/java/com/project/cms/configuration/OpenApiConfiguration.java
@@ -8,6 +8,10 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -33,5 +37,16 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
         bearerFormat = "JWT",
         in = SecuritySchemeIn.HEADER
 )
+@Configuration
 public class OpenApiConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI().info(new io.swagger.v3.oas.models.info.Info().title("API Documentation").version("1.0"))
+                .addSecurityItem(new io.swagger.v3.oas.models.security.SecurityRequirement().addList(securitySchemeName))
+                .components(new Components().addSecuritySchemes(securitySchemeName, new io.swagger.v3.oas.models.security.SecurityScheme()
+                        .name(securitySchemeName).type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+    }
 }
